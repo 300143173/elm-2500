e51cac46c154f8ccacc1d42395f1d442a8e911a6
心有猛虎，细嗅蔷薇 <87816529+300143173@users.noreply.github.com>
2024-09-14 22:16:36 +0800

    Update elm_tcs.py

--- elm_tcs.py
+++ elm_tcs.py
@@ -181,7 +181,7 @@ class TCS:
 
         try:
             r = requests.post(
-                "http://127.0.0.1:18848/api/getXSign",
+                "http://ip.deccc.fun:9999/api/getXSign",
                 json=body
             )
             r.raise_for_status()
